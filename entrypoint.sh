2899e8e645c16bbea184910c3326ad5f37fd0877
itwhs <itwhs@foxmail.com>
2022-07-14 18:47:11 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ if [ ! -e '/var/www/html/public/index.php' ]; then
     chgrp -R www-data /var/www/lsky
     chmod -R 755 /var/www/lsky
     cp -ar /var/www/lsky/* /var/www/html/
-    cp -ar /var/www/lsky/.env.example /var/www/html
+    cp -ar /var/www/lsky/.env* /var/www/html
 fi
 
 exec "$@"
