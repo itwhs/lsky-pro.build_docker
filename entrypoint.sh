83c1c8e1d2f3a82b05475b51001452c371a824cc
itwhs <itwhs@foxmail.com>
2022-07-14 19:25:41 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,9 @@ if [ ! -e '/var/www/html/public/index.php' ]; then
     chown -R www-data /var/www/lsky
     chgrp -R www-data /var/www/lsky
     chmod -R 755 /var/www/lsky
-    cp -ar /var/www/lsky/* /var/www/html/
-    cp -ar /var/www/lsky/.env* /var/www/html
+    cp -ar /var/www/lsky /var/www/html
+    rm -rf /var/www/html/.env
 fi
+echo "本次未执行初始化操作.默认http为:http://`tail -n1 /etc/hosts |awk '{print$1}'`:80"
 
 exec "$@"
